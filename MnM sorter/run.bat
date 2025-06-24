2153b04df8272fd287219f0bb6030026555ac47a
StrongHigh <4ura02@gmail.com>
2025-06-24 20:24:26 +0900

    Fix: change directory name

--- MnM sorter/run.bat
+++ MnM sorter/run.bat
@@ -9,12 +9,10 @@ if not exist %VENV_DIR%\Scripts\activate.bat (
 )
 call %VENV_DIR%\Scripts\activate.bat
 
-:: 4. pip 최신화 및 패키지 설치
 echo Installing requirements...
 python -m pip install --upgrade pip
 pip install -r requirements.txt
 
-:: 5. 스크립트 실행 (main.py 등)
 echo Running main.py...
 python main.py
 
